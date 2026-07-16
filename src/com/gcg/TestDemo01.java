1d8001bedad6fe8bfb26fa9225b61860a2585336
satti <satti@Santhoshi>
2026-07-16 15:37:33 +0530

    commit

--- src/com/gcg/TestDemo01.java
+++ src/com/gcg/TestDemo01.java
@@ -5,7 +5,7 @@ public class TestDemo01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Kalyan Reddy");
+		System.out.println("Kalyan Reddy 01");
 
 	}
 
